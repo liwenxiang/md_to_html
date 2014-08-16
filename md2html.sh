c1e81325de90f9eafc2d1ad58218b1d487783735
codeboy <1021320645@qq.com>
2014-08-16 20:45:45 +0800

    避免使用绝对路径

--- md2html.sh
+++ md2html.sh
@@ -1,4 +1,4 @@
 curDir="/Users/liwenxiang/Program/md_to_html/"
 echo `cat $curDir/theme/head.html`
-node /Users/liwenxiang/Program/md_to_html/md2html.js "$1"
+node $curDir/md2html.js "$1"
 echo `cat $curDir/theme/end.html`
